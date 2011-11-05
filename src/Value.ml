0f4b7095505e92ba8db23ecb4c689a2d61d48367
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2011-11-06 05:34:16 +0900

    Refactored Value.ml to arrange into formal source code regulations.

--- src/Value.ml
+++ src/Value.ml
@@ -1,6 +1,6 @@
 
 type t =
-    Nil
+  | Nil
   | Bool of bool
   | Int of Num.num
   | Float of float
@@ -24,29 +24,29 @@ let bool_of_value = function
 let sprintf = Printf.sprintf
 
 let rec to_string = function
-    Nil -> "nil"
-  | Bool b -> if b then "true" else "false"
+  | Nil -> "nil"
+  | Bool true -> "true"
+  | Bool false -> "false"
   | Int n -> Num.string_of_num n
   | Float f -> string_of_float f
   | String s -> s
   | Array a ->
       let strings = Array.to_list (Array.map to_string a) in
-      "[" ^ (String.concat ", " strings) ^ "]"
+      sprintf "[%s]" (String.concat ", " strings)
   | Dict h ->
       let string_of_pair key value init =
-        let pair = (to_string key) ^ ": " ^ (to_string value) in
-        pair :: init in
+        (sprintf "%s: %s" (to_string key) (to_string value)) :: init in
       let pairs = Hashtbl.fold string_of_pair h [] in
       if (List.length pairs = 0) then
         "{}"
       else
-        "{ " ^ (String.concat ", " pairs) ^ " }"
-  | Function _ -> "Function"
-  | Method _ -> "Method"
-  | UserFunction _ -> "UserFunction"
+        sprintf "{ %s }" (String.concat ", " pairs)
+  | Function _ -> "<Function>"
+  | Method _ -> "<Method>"
+  | UserFunction _ -> "<UserFunction>"
   | ProcessStatus stat -> string_of_int stat
-  | Class (name, _) -> sprintf "#<Class %s>" name
-  | Exception (Class (name, _), _, _) -> sprintf "#<%s>" name
+  | Class (name, _) -> sprintf "<Class %s>" name
+  | Exception (Class (name, _), _, _) -> sprintf "<%s>" name
   | _ -> assert false
 
 (*
